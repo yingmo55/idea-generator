b49d5edb8d10424f1683b22e5887a029c81dcdeb
Ying <97061798+yingmo55@users.noreply.github.com>
2022-01-20 15:17:08 -0500

    add more ideas

--- ideas.js
+++ ideas.js
@@ -2,13 +2,14 @@ const ideaLogs = {
     0: 'make a random idea generator',
     1: 'make a calculator',
     2: 'make a photography website with slide shows',
-    3: 'Make a landing page',
-    4: 'Make a to-do list app',
+    3: 'make a landing page',
+    4: 'make a to-do list app',
     5: 'make something using an API',
     6: 'build a quiz game using JavaScript',
     7: "an HTML form",
     8: "tic tac toe game",
-    9: "a weather app"
+    9: "a weather app",
+    10: 'something with CSS animation'
 }
 
 export default ideaLogs
