e5ae3abfc39e07fc2183ef185e26b928b2b6c052
Ying <97061798+yingmo55@users.noreply.github.com>
2022-01-20 00:24:51 -0500

    add feature to prevent getting the same number twice in a row

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
 const idea = document.getElementById('card-text');
 const ideaButton = document.getElementById('ideaGenerator');
 const ideaHeader = document.getElementById('ideaHeader');
+const doNotShowBtn = document.getElementById('doNotShow');
 
 const ideaLogs = {
     0: 'make a random idea generator',
     1: 'make a calculator',
-    2: 'make a photography website with slide shows'
+    2: 'make a photography website with slide shows',
+    3: 'Make a landing page',
+    4: 'Make a to-do list app',
+
 }
 
-
+const doNotShow = [];
+let lastNum;
 function randomIdea(){
-    let randomNum = Math.floor(Math.random() * 2)
+    if (doNotShow.length === ideaLogs.length) {
+        return 'Actually, I am out of ideas. Do you have an idea to show?'
+    }
+    let randomNum = Math.floor(Math.random() * Object.keys(ideaLogs).length)
+    while (lastNum === randomNum) {
+        randomNum = Math.floor(Math.random() * Object.keys(ideaLogs).length);
+    }
+    // console.log(`random number: ${randomNum}, last number: ${lastNum}`);
+    lastNum = randomNum;
     return ideaLogs[randomNum];
+
 }
 
 ideaButton.onclick = () => {
     ideaHeader.innerHTML = 'How about...';
     idea.innerHTML = randomIdea();
+    doNotShowBtn.style.display = 'inline-block';
 }
