561d872ceef29509957f3ca45a7f06aa0b13809d
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-06 16:26:23 -0500

    Change format of ideaLog. Change ideaLogs from an object to array Adjust wording for some elements Add more ideas

--- ideas.js
+++ ideas.js
@@ -1,15 +1,18 @@
-const ideaLogs = {
-    0: 'make a random idea generator',
-    1: 'make a calculator',
-    2: 'make a photography website with slide shows',
-    3: 'make a landing page',
-    4: 'make a to-do list app',
-    5: 'make something using an API',
-    6: 'build a quiz game using JavaScript',
-    7: "an HTML form",
-    8: "tic tac toe game",
-    9: "a weather app",
-    10: 'something with CSS animation'
-}
+const ideaLogs = [
+    'a random idea generator',
+    'a calculator',
+    'a photography website with slide shows',
+    'a landing page',
+    'a to-do list app',
+    'something that uses an API',
+    'a quiz game using JavaScript only',
+    "an HTML form",
+    "a tic tac toe game",
+    "a weather app",
+    'something with CSS animation',
+    "a personal site",
+    "a clone page of another website",
+    "a pomodoro timer",
+    "a countdown timer"]
 
 export default ideaLogs
